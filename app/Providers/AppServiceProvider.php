00561bde2c99f288c5f1b2e9157067938b873bdc
Paulo <paulopuyot@gmail.com>
2026-04-29 21:57:50 +0800

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+      if (str_contains(config('app.url'), 'railway.app')) {
             URL::forceScheme('https');
         }
     }
